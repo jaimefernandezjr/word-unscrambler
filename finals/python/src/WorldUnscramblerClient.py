fbbd7dcee4ab2f589b170590a635363e0e50a1c0
dajen_torralba <2205205@slu.edu.ph>
2021-05-25 14:46:48 +0800

    authored initial python client

---
+++ finals/python/src/WorldUnscramblerClient.py
@@ -0,0 +1,46 @@
+import CORBA
+import WordUnscrambler
+import CosNaming
+import sys
+
+orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+
+obj = orb.resolve_initial_references("NameService")
+rootContext = obj._narrow(CosNaming.NamingContext)
+
+if rootContext is None:
+    print ("Failed to narrow the root naming context")
+    sys.exit(1)
+
+name = [CosNaming.NameComponent("WordUnscrambler", ""), ]
+try:
+    obj = rootContext.resolve(name)
+except CosNaming.NamingContext.NotFound as ex:
+    print ("Name not found")
+sys.exit(1)
+
+wordUnscrambler = obj._narrow(WordUnscramblerApp.WordUnscrambler)
+
+if wordUnscrambler is None:
+    print ("Object reference is not an WordUnscramblerApp::WordUnscrambler")
+    sys.exit(1)
+
+print("----------------------------")
+print("Welcome to Word Unscrambler!")
+print("----------------------------\n")
+
+print("1 - Login\n"
+      "2 - Register\n")
+
+choice = input("Choice: ")
+
+if choice == 1:
+    print("login")
+elif choice == 2:
+    print ("register")
+else:
+    print ("invalid")
+
+
+
+
